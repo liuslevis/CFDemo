ce6cf053979a92ed14bf004acc81ed53208517a5
richliu(刘笑江) <richliu@tencent.com>
2017-03-06 13:35:13 +0800

    init

--- word2vec.py
+++ word2vec.py
@@ -40,15 +40,18 @@ def flatMap(vocab):
                 ret.append(j)
     return ret
 
+def calc_item_cf():
+    sents = sents_from_prefs(prefs) # LineSentence('README.md')
+    vocab = [s.split() for s in sents]
+    model = Word2Vec(vocab, size=100, window=5, min_count=1, workers=4)
+    model.save_word2vec_format(model_file, binary=False)
+    model = Word2Vec.load_word2vec_format(model_file, binary=False)
 
-sents = sents_from_prefs(prefs) # LineSentence('README.md')
-vocab = [s.split() for s in sents]
-model = Word2Vec(vocab, size=100, window=5, min_count=1, workers=4)
-model.save_word2vec_format(model_file, binary=False)
-model = Word2Vec.load_word2vec_format(model_file, binary=False)
+    print('基于书籍的 word2vec 协同过滤推荐')
+    for item in flatMap(vocab):
+        print('\n根据 %s 推荐：' % item)
+        for item_score in model.most_similar(positive=[item]):
+            item, score = item_score
+            print('\t%s %.2f' % (item, score))
 
-for item in flatMap(vocab):
-    print('\n根据书籍 %s 推荐：' % item)
-    for item_score in model.most_similar(positive=[item]):
-        item, score = item_score
-        print('\t%s %.2f' % (item, score))
+calc_item_cf()
